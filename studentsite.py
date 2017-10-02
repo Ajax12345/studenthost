cab034f548ae0d840012dd4b5c636ac81b2bee34
Ajax1234 <pentester18@gmail.com>
2017-10-02 19:55:14 -0400

    Update studentsite.py

--- studentsite.py
+++ studentsite.py
@@ -8,10 +8,20 @@ class MainHandler(tornado.web.RequestHandler):
 
         self.render('/Users/davidpetullo/Documents/studentscout/home.html')
 
+class StudentRegster(tornado.web.RequestHandler):
+    def get(self):
+        self.render("/Users/davidpetullo/Documents/studentscout/student_register.html")
+
+    def post(self):
+        username = self.get_body_argument("username", default=None, strip=False)
+        password = self.get_body_argument("password", default=None, strip=False)
+        print (username, password)
+
 def make_app():
     settings = {"static_path": os.path.join(os.path.dirname(__file__), "student_style")}
     return tornado.web.Application([
         (r"/", MainHandler),
+        (r'/student_register', StudentRegster)
     ], **settings)
 
 if __name__ == "__main__":
